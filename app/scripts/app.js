4bba1847e7880424fd0cf85f3ba13e90ac1d7e8c
bev-a-tron <beverly.a.lau@gmail.com>
2014-11-17 13:33:07 -0800

    [BL] Add experience view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/experience', {
+        templateUrl: 'views/experience.html',
+        controller: 'ExperienceCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/experience.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mywebsiteApp')
+  .controller('ExperienceCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
